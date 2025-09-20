595b1a19a2861609d8211e47e17f75d78fcc6f0f
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-09-19 23:43:05 -0500

    Actividad 2 del laboratorio, con la extencion de bloquear los botones

--- app/src/main/java/com/example/actividad2/MainActivity.kt
+++ app/src/main/java/com/example/actividad2/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +23,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.actividad2.ui.theme.Actividad2Theme
@@ -37,6 +40,8 @@ class MainActivity : ComponentActivity() {
 fun contador(){
     var countEphemeral by remember { mutableStateOf(0) }
     var countPersistente by rememberSaveable { mutableStateOf(0) }
+    var bloqueado by rememberSaveable { mutableStateOf(false) }
+
 
     Column (
         modifier = Modifier
@@ -46,7 +51,8 @@ fun contador(){
             "Ephimeral(remember): $countEphemeral"
         )
         Button(
-            onClick = { countEphemeral++ }
+            onClick = { countEphemeral++ },
+            enabled = !bloqueado
         ) {
             Text("+1")
         }
@@ -57,7 +63,8 @@ fun contador(){
             "Persistente (remberSaveable): $countPersistente"
         )
         Button(
-            onClick = { countPersistente++ }
+            onClick = { countPersistente++ },
+            enabled = !bloqueado
         ) {
             Text("+1")
         }
@@ -65,12 +72,25 @@ fun contador(){
         Spacer(modifier = Modifier.height(16.dp))
         Text("🔄 Rota la pantalla y observa qué contador se reinicia.")
 
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text("Bloquear")
+            Switch(
+                checked = bloqueado,
+                onCheckedChange = { bloqueado = it }
+            )
+        }
 
-
+        // Mensaje en rojo cuando está bloqueado
+        if (bloqueado) {
+            Text(
+                text = "Contador bloqueado",
+                color = Color.Red,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
     }
-
 }
-
 
 
 
